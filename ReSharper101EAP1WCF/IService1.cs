2dbfc5d7d78ca7465269b3d51affb28226f04974
gorohoroh <gorohoroh@gmail.com>
2016-02-04 17:16:16 +0300

    WCF-specific actions

---
+++ ReSharper101EAP1WCF/IService1.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace ReSharper101EAP1WCF
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        // UPDATE: Inspection "WCF operation marked as one way with return value" + QF
+        [OperationContract(IsOneWay = true)]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+    }
+
+    // UPDATE: Inspection "WCF service doesn't contain any OperationContracts" + QF
+    [ServiceContract]
+    interface IService2
+    {
+        string GetData(int value);
+
+    }
+    
+    // UPDATE: Inspection "WCF OperationContract can be placed only inside a type marked as ServiceContract" + QF
+    interface IService3
+    {
+        [OperationContract]
+        string GetData(int value);
+
+    }
+
+    // UPDATE: CA "Annotate public fields and properties with [DataMember]"
+    [DataContract]
+    public class CompositeType
+    {
+        public bool BoolValue { get; set; } = true;
+
+        public string StringValue { get; set; } = "Hello ";
+
+        public int IntValue { get; set; } = 12532;
+    }
+
+
+    // UPDATE: CA "Annotate enum members with [EnumMember]"
+    [SuppressMessage("ReSharper", "InconsistentNaming"), 
+     SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [DataContract]
+    enum MicrosoftAcronyms
+    {
+        ADONET,
+        ASPNET,
+        ASPNETMVC,
+        CDONTS,
+        CSPDK,
+        DACL,
+        DNA,
+        UWP,
+        WCF,
+        WPF,
+        WWF
+    }
+}
---
+++ ReSharper101EAP1WCF/Service1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReSharper101EAP1WCF
+{
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
