a776264b3dfdffc4ea7d347f66df0bbefb1c752e
gorohoroh <gorohoroh@gmail.com>
2016-02-05 00:46:53 +0300

    Initial commit of C++ project for EAP1 + .gitignore update

---
+++ ReSharperCpp101EAP1/ReSharperCpp101EAP1.cpp
@@ -0,0 +1,12 @@
+#include "ReSharperCpp101EAP1.h"
+
+
+
+ReSharperCpp101EAP1::ReSharperCpp101EAP1()
+{
+}
+
+
+ReSharperCpp101EAP1::~ReSharperCpp101EAP1()
+{
+}
---
+++ ReSharperCpp101EAP1/ReSharperCpp101EAP1.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+
+class ReSharperCpp101EAP1
+{
+	// UPDATE: QF "Create and initialize field from constructor parameter
+	ReSharperCpp101EAP1(int x, char lalala)
+	{
+	}
+
+	ReSharperCpp101EAP1();
+	~ReSharperCpp101EAP1();
+
+
+};
+
