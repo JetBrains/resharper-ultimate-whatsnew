394a82a682b8798328cc72d0539f05bf6175684c
Dmitry.Matveev <dmitry.matveev@jetbrains.com>
2016-03-15 17:02:24 +0100

    New features for 10.1 EAP 6

---
+++ ReSharper101CSharp/ArgumentStyle.cs
@@ -0,0 +1,26 @@
+﻿// ReSharper disable UnusedParameter.Local
+
+// TODO: 101EAP6: Code style for named/positional arguments
+
+namespace ReSharper101CSharp
+{
+  class ArgumentStyle
+  {
+    void AddCheckbox(bool enabled, bool value, string label, string tooltip)
+    {
+     
+    }
+
+    void Test()
+    {
+
+        AddCheckbox(true, true, "AutoFill", null);
+
+
+
+
+
+
+    }
+  }
+}
---
+++ ReSharper101CSharp/BracesForSingleLinersStyle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReSharper101CSharp
+{
+  class BracesForSingleLinersStyle
+  {
+
+    //TODO: 101EAP6 Code style for braces around single-line statements
+
+    void Test(object arg)
+    {
+
+         if (arg == null)  throw new ArgumentNullException(nameof(arg));
+
+
+
+
+
+
+
+
+    }
+  }
+}
