044d8fe851c1afde72f0d3593bf69cbcb1de6821
Dmitry.Matveev <dmitry.matveev@jetbrains.com>
2016-06-10 13:27:25 +0200

    New features for 2016.2 EAP1

---
+++ ReSharper20162CSharp/ReSharper 2016.2/ReSharper20162CSharp/DotCompletion.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace ReSharper20162CSharp
+{
+    public class DotCompletion
+    {
+    //20162EAP1: Typing assistance: dot/semicolon inside parentheses, dot after semicolon
+    private string Foo(StringBuilder myStringBuilder)
+    {
+      //myStringBuilder.AppendLine(.)
+        return null;
+      }
+    }
+}
---
+++ ReSharper20162CSharp/ReSharper 2016.2/ReSharper20162CSharp/MoveNestedToFileRefacrtoring.cs
@@ -0,0 +1,12 @@
+namespace ReSharper20162CSharp.ReSharper_2016._2.ReSharper20162CSharp
+{
+  class MoveNestedToFileRefacrtoring
+  {
+    //20162EAP1: The 'Move to Another File' refactoring is now available for nested types
+    class Nested
+    {
+      
+    }
+
+  }
+}
