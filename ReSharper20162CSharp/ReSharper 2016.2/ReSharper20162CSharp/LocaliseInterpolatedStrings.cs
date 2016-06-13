55befa449261bcac39ee54dfc4ae932d54caddaa
Matt Ellis <m.t.ellis@gmail.com>
2016-06-13 23:21:37 +0100

    Localisation for interpolated strings

---
+++ ReSharper20162CSharp/ReSharper 2016.2/ReSharper20162CSharp/LocaliseInterpolatedStrings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ReSharper20162CSharp.ReSharper_2016._2.ReSharper20162CSharp
+{
+    public class LocaliseInterpolatedStrings
+    {
+        public void Say(string message)
+        {
+            // Non-localised interpolated string, highlighted as warning
+            // "Move to resource" available as both quick fix and via refactoring
+            Console.WriteLine($"Hello {message}");
+        }
+    }
+}
