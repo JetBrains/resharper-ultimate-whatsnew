41e202d6917564a3b103f6dbbc4cb6d0ff59b9fe
Matt Ellis <m.t.ellis@gmail.com>
2016-06-13 22:38:06 +0100

    VB string interpolation

--- ReSharper20162CSharp/ReSharper 2016.2/ReSharper20162CSharp/SuppressSsrHighlight.cs
+++ ReSharper20162CSharp/ReSharper 2016.2/ReSharper20162CSharp/SuppressSsrHighlight.cs
@@ -8,6 +8,7 @@ namespace ReSharper20162CSharp.ReSharper_2016._2.ReSharper20162CSharp
         {
             // SSR pattern from solution will flag DateTime.Now as a suggestion
             // Alt+Enter to allow "Disable once with comment"
+            // ReSharper disable once MyDateTimeNow
             var dateTime = DateTime.Now;
             Console.WriteLine(dateTime);
         }
---
+++ ReSharper20162VB/StringInterpolation.vb
@@ -0,0 +1,15 @@
+﻿Public Class StringInterpolation
+    Sub ConvertStringFormat(name As String)
+        Dim message = String.Format("Hello {0}", name)
+        Console.WriteLine(message)
+    End Sub
+
+    Function ConvertConcatenation(forename As String, surname As String) As String
+        return forename + surname
+    End Function
+
+    Function ConvertStringLiteral()
+        ' Context action on string literal to convert to $"..."
+        return "Hello world"
+    End Function
+End Class
