55875b42b912a7b86047491ca08e4c296aa86128
Matt Ellis <m.t.ellis@gmail.com>
2016-06-13 23:48:39 +0100

    Fix invalid escape chars

---
+++ ReSharper20162CSharp/ReSharper 2016.2/ReSharper20162CSharp/FixInvalidEscapeChars.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ReSharper20162CSharp.ReSharper_2016._2.ReSharper20162CSharp
+{
+    public class FixInvalidEscapeChars
+    {
+        public void InvalidEscapeChars()
+        {
+            // Fix invalid escape chars by 
+            Console.WriteLine("C:\invalid\chars");
+            Console.WriteLine("C:\more\invalid\chars");
+        }
+    }
+}
