eb0e66a3d0f1c56af20a5383c79a6be347460d71
Matt Ellis <m.t.ellis@gmail.com>
2016-06-13 21:35:45 +0100

    Suppress SSR highlights

---
+++ ReSharper20162CSharp/ReSharper 2016.2/ReSharper20162CSharp/SuppressSsrHighlight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReSharper20162CSharp.ReSharper_2016._2.ReSharper20162CSharp
+{
+    public class SuppressSsrHighlight
+    {
+        public void SuppressHighlightOfLocalisedDateTimeNow()
+        {
+            // SSR pattern from solution will flag DateTime.Now as a suggestion
+            // Alt+Enter to allow "Disable once with comment"
+            var dateTime = DateTime.Now;
+            Console.WriteLine(dateTime);
+        }
+    }
+}
