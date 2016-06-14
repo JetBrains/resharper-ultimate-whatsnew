29f6c3b8d3620046baf012d64b51ac4e89fa3898
Matt Ellis <m.t.ellis@gmail.com>
2016-06-14 21:07:48 +0100

    C++ inline variable

---
+++ ReSharper20162Cpp/inlineVariable.cpp
@@ -0,0 +1,16 @@
+﻿class InlineVariable
+{
+    static int getValue(int input1, int input2)
+    {
+        auto result = input1 + input2;
+        // Place caret on `result` below, invoke Refactor This → Inline Variable
+        return result;
+    }
+
+    static int getValueSquared(int input1, int input2)
+    {
+        auto result = input1 + input2;
+        // Place caret on `result` below, invoke Refactor This → Inline Variable
+        return result * result;
+    }
+};
