2f843e38ddb5b71f7fe515950712682c8cf1ad13
Matt Ellis <m.t.ellis@gmail.com>
2016-06-15 11:01:14 +0100

    VB string interpolation requries Option Strict On
    It also requires sensible code, but we won't talk about that.

--- ReSharper20162VB/StringInterpolation.vb
+++ ReSharper20162VB/StringInterpolation.vb
@@ -1,14 +1,17 @@
-﻿Public Class StringInterpolation
+﻿Option Strict On
+
+Public Class StringInterpolation
     Sub ConvertStringFormat(name As String)
         Dim message = String.Format("Hello {0}", name)
         Console.WriteLine(message)
     End Sub
 
     Function ConvertConcatenation(forename As String, surname As String) As String
-        return forename + surname
+        ' Alt+Enter on concatenation below and "Convert concatenation to interpolation"
+        return "Hello " & forename & surname
     End Function
 
-    Function ConvertStringLiteral()
+    Function ConvertStringLiteral() As String
         ' Context action on string literal to convert to $"..."
         return "Hello world"
     End Function
