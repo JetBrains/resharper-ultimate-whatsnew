b8618f20a57e34cc78ad1a916b24347a7f5ceb30
gorohoroh <gorohoroh@gmail.com>
2016-08-12 20:13:50 +0300

    2016.2 EAP4: Doxygen comments (C++)

---
+++ ReSharper20162CppUniversal/Doxygen.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+// 20162EAP4: Find Usages and Rename takes into account usages in Doxygen comments
+class DoxygenComments
+{
+public:
+	/*!
+	a normal member taking two arguments and returning an integer value.
+	\param a an integer argument.
+	\param s a constant character pointer.
+	\return The test results
+	*/
+	int qtStyle(int a, const char *s);
+
+	/**
+	* a normal member taking two arguments and returning an integer value.
+	* @param a an integer argument.
+	* @param s a constant character pointer.
+	* @return The test results
+	*/
+	int javaDocStyle(int a, const char *s);
+};
