fa31647f83573cbe23786a87c4dd78ba8af0eecb
gorohoroh <gorohoroh@gmail.com>
2016-08-12 20:58:17 +0300

    Suppress SSR with comment + dotsettings (SSR pattern, JSON value helper), all based on Matt's changes

---
+++ ReSharper20162CSharp/SuppressSsrHighlight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReSharper20162CSharp
+{
+    public class SuppressSsrHighlight
+    {
+        public void SuppressHighlightOfLocalisedDateTimeNow()
+        {
+            // 20162EAP4: SSR pattern from solution will flag DateTime.Now as a suggestion
+            // Alt+Enter to allow "Disable once with comment"
+            var dateTime = DateTime.Now;
+            Console.WriteLine(dateTime);
+        }
+    }
+}
