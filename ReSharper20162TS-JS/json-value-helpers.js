469b1552b124f17eb3283e8e69277966244a2ab7
gorohoroh <gorohoroh@gmail.com>
2016-08-15 20:52:32 +0300

    Completion based on JSON value helpers (based on Matt's contributions)

---
+++ ReSharper20162TS-JS/json-value-helpers.js
@@ -0,0 +1,4 @@
+﻿// 20162RTM: JSON value helpers. See tsconfig.json for an actual sample
+// .sln.dotSettings has a value helper for tsconfig.json files to treat compilerOptions:rootDir as a path.
+// (Settings are under ReSharper > Options > Code Editing > JSON > JSON Value Helpers)
+// In tsconfig.json, invoke completion at rootDir value to see path suggestions
