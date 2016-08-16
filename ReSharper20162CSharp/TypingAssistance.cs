4c449ddcfc8ce89b48cf58cc1f4e141e75be3e81
gorohoroh <gorohoroh@gmail.com>
2016-08-16 17:44:19 +0300

    C# typing assistants hopefully made clearer

---
+++ ReSharper20162CSharp/TypingAssistance.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace ReSharper20162CSharp
+{
+    public class TypingAssistance
+    {
+        private string Foo(StringBuilder myStringBuilder)
+        {
+            // 20162RTM: Typing assistance: dot/semicolon inside parentheses, dot after semicolon 
+            // (a chain of all three shown here: https://twitter.com/controlflow/status/734508760761270272/)
+
+            // 1. dot inside parentheses: 
+            //            myStringBuilder.AppendLine(.) -> myStringBuilder.AppendLine().{caret}
+
+            // 2. Semicolon inside parentheses: 
+            //            myStringBuilder.AppendLine(;) -> myStringBuilder.AppendLine({caret});
+
+            // 3. dot after semicolon: 
+            //            myStringBuilder.AppendLine().Capacity;. -> myStringBuilder.AppendLine().Capacity.{caret};
+
+            return null;
+        }
+    }
+}
