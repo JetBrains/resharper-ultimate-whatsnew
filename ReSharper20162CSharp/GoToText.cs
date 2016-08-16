0fb1b7b33f0f591e1b7577b03dbce5381467c291
gorohoroh <gorohoroh@gmail.com>
2016-08-16 20:01:02 +0300

    Go to Text

---
+++ ReSharper20162CSharp/GoToText.cs
@@ -0,0 +1,8 @@
+﻿namespace ReSharper20162CSharp
+{
+    public class GoToText
+    {
+        // 20162RTM: Go to Text (Ctrl+T,T,T in Visual Studio keymap) finds words and phrases across a variety of files in solution
+        // In Go to Text, enter "Visual", then expand to "Visual Studio"
+    }
+}
