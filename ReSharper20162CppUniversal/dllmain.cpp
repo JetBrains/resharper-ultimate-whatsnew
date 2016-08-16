3610ca47ddc48f66d9d903853d293d01e9b429fa
gorohoroh <gorohoroh@gmail.com>
2016-08-16 21:15:41 +0300

    С++: auto-highlighting returns; QF to generate a missing default case in a switch

--- ReSharper20162CppUniversal/dllmain.cpp
+++ ReSharper20162CppUniversal/dllmain.cpp
@@ -2,13 +2,23 @@
 
 BOOL APIENTRY DllMain(HMODULE /* hModule */, DWORD ul_reason_for_call, LPVOID /* lpReserved */)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+	if (ul_reason_for_call > 4294967295)
+	{
+		// 20162RTM: Caret at return keyword will auto-highlight all function exits
+		return FALSE;
+	}
+
+	else
+	{
+		// 20162RTM: Quick-fix to generate a missing default case in a switch statement
+		switch (ul_reason_for_call)
+		{
+		case DLL_PROCESS_ATTACH:
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+		case DLL_PROCESS_DETACH:
+			break;
+		}
+		return TRUE;
+	}
 }
