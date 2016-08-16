ccb81fc81ed50243b6f95162dcfe6f62c9023270
gorohoroh <gorohoroh@gmail.com>
2016-08-16 18:59:43 +0300

    Extract Members to Partial: made sample code easier

---
+++ ReSharper20162CSharp/ExtractMembersToPartialRefactoring.cs
@@ -0,0 +1,31 @@
+﻿namespace ReSharper20162CSharp
+{
+    // 20162RTM: Extract Members to Partial refactoring (moves members between class parts; creates new parts if necessary): 3 scenarios
+
+    // Refactor This on class name to decide which members to move to another part
+    partial class PartialClass
+    {
+        // Refactor This on member name to move this member to another part
+        public int IntegerProperty { get; set; }
+
+        // Refactor This on region name to extract all members in the region to a separate part
+
+        #region MyRegion
+
+        public string StringProperty1 { get; set; }
+        public string StringProperty2 { get; set; }
+
+        #endregion
+
+        public PartialClass(string stringProperty1, string stringProperty2)
+        {
+            StringProperty1 = stringProperty1;
+            StringProperty2 = stringProperty2;
+        }
+    }
+
+    partial class PartialClass
+    {
+        public string StringProperty3 { get; set; }
+    }
+}
