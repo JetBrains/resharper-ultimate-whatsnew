e9277f8417898abdb877f7007e50f32e53fbbd9e
gorohoroh <gorohoroh@gmail.com>
2016-08-16 20:18:07 +0300

    CA to generate doc stubs in C++

---
+++ ReSharper20162CppUniversal/GenerateComments.cpp
@@ -0,0 +1,7 @@
+﻿#include "pch.h"
+
+// 20162RTM: Context action to generate document comments for C++ symbols (Alt+Enter on method name)
+static int undocumentedMethod(int a, const char* s)
+{
+	return 0;
+}
