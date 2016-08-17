dbf1100334e2d921f0b808f3857d37db4f08c837
Anton Lobov <Anton.Lobov@jetbrains.com>
2016-08-17 10:58:05 +0200

    update ts2016.2 sample: generate missing members abstract, generate missing literal properties, generative completion with lambdas

--- ReSharper20162TS-JS/TypeScript2016.2.ts
+++ ReSharper20162TS-JS/TypeScript2016.2.ts
@@ -1,14 +1,17 @@
-﻿
+﻿interface Animal {
+    makeSound(): void;
 
-abstract class Animal {
-    abstract makeSound(): void;
-
-    abstract walk(): void;
+    walk(): void;
 }
 
-// 20162RTM: Abstract members generation in TypeScript. Apply quick-fix on Animal to implement missing members
-class Dog extends Animal {
-
-    // 20162RTM: Generative completion in TypeScript. Ctrl+Space here for override options
-
+// 20162RTM: Abstract members generation in TypeScript. Apply quick-fix on Animal to implement missing members / implement missing members abstract (the last one is new)
+abstract class Dog implements Animal {
+    // 20162RTM: Generative completion in TypeScript. Ctrl+Space here or start typing "over" for override options
 }
+ 
+var kitty: Animal = { 
+  // 20162RTM: Generate missing object literal properties
+};
+
+declare function invokeCallback(func: (a: string, b: number) => boolean);
+invokeCallback( /*20162RTM: invoke completion here, see lambdas and functions*/  );
