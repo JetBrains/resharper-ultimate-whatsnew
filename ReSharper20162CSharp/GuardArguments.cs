08ffa41bce8fd323903a675a9eeec9ccee6dff34
hadihariri <mail@hadihariri.com>
2016-08-23 16:43:54 +0200

    Added parameter that can't be null to demonstrate feature better

--- ReSharper20162CSharp/GuardArguments.cs
+++ ReSharper20162CSharp/GuardArguments.cs
@@ -5,7 +5,7 @@ namespace ReSharper20162CSharp
     public class GuardArguments
     {
         // 20162RTM: Alt+Enter on parameter "Check parameter for null", expand to check all parameters at once
-        public void NullGuardForAllArguments(int? intValue, string stringValue, object o)
+        public void NullGuardForAllArguments(int? intValue, string stringValue, object o, int notNull)
         {
         }
     }
