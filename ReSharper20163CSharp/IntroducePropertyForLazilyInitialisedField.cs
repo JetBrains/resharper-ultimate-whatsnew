48ce55af8d317251188b1c258b185865a3a00178
Matt Ellis <m.t.ellis@gmail.com>
2016-11-28 17:06:25 +0000

    Add introduce property for lazy example

---
+++ ReSharper20163CSharp/IntroducePropertyForLazilyInitialisedField.cs
@@ -0,0 +1,17 @@
+﻿using System;
+// ReSharper disable UnusedMember.Global
+
+namespace ReSharper20163CSharp
+{
+  public class IntroducePropertyForLazilyInitialisedField
+  {
+    // Introduce property for lazily initialised field
+    //
+    // Alt+Enter and select "Introduce property for lazily initialized field
+    // Introduces a property to make the field easier to use - just use the property
+    // instead of calling _foo.Value
+
+
+    private readonly Lazy<string> _foo = new Lazy<string>(() => "Hello world");
+  }
+}
