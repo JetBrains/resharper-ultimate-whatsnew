9776e2940c8927262a301067558828f09944ab53
Matt Ellis <m.t.ellis@gmail.com>
2016-11-28 16:55:02 +0000

    Add generate ctor check for null example

---
+++ ReSharper20163CSharp/GenerateConstructorCheckForNull.cs
@@ -0,0 +1,18 @@
+﻿// ReSharper disable UnusedMember.Global
+#pragma warning disable 169
+namespace ReSharper20163CSharp
+{
+  public class GenerateConstructorCheckForNull
+  {
+    private string _stuff;
+    private string _stuff2;
+    private string _stuff3;
+
+    // Generate Constructor now includes option to check for null
+    //
+    // Invoke Generate menu (Alt+Ins) → Constructor
+    // Select fields to generate parameters for
+    // Select "Check parameters for null"
+
+  }
+}
