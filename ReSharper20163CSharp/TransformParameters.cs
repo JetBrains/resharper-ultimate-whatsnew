825a8ddd2e16dc9d6a6a694d514762aa09ca14e4
Matt Ellis <m.t.ellis@gmail.com>
2016-11-28 16:31:06 +0000

    Add Transform Parameters refactoring example

---
+++ ReSharper20163CSharp/TransformParameters.cs
@@ -0,0 +1,17 @@
+﻿namespace ReSharper20163CSharp
+{
+  public class TransformParameters
+  {
+    // Transform Parameters refactoring
+    //
+    // Converts incoming parameters to new class or tuple. Converts out parameters and return value to
+    // new class or tuple
+    // Invoke Refactor This on method signature (method name or parameters) and select Transform Parameters
+    public bool DoSomething(int param1, string param2, int param3, out bool param4)
+    {
+      param4 = true;
+
+      return false;
+    }
+  }
+}
