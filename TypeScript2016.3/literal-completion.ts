13c07b2da751a1b2801cf0afec359b6a4fbe26e1
Matt Ellis <m.t.ellis@gmail.com>
2016-11-29 09:44:31 +0000

    Add TypeScript literal completion example

---
+++ TypeScript2016.3/literal-completion.ts
@@ -0,0 +1,20 @@
+﻿// Literal type completion
+//
+// Given a type defined as a literal, provide each literal value as a completion item
+
+function escapeDigit(d: 0|1|2|3|4|5|6|7|8|9, base) {
+    // ...
+}
+
+// Invoke completion on first parameter
+escapeDigit(0, 10);
+
+
+
+
+function processStatus(status: "success" | "fail") {
+    // ...
+}
+
+// Invoke completion inside or outside of the quotes
+processStatus("fail");
