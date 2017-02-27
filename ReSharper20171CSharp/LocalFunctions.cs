d0f28dfa7015847bba473490d0aa241a185cfa06
Svetlana <svetlana.emelyanova@jetbrains.com>
2017-02-27 13:12:18 +0300

    Added ReSharper2017.1

---
+++ ReSharper20171CSharp/LocalFunctions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace ReSharper20171CSharp
+{
+    class LocalFunctions
+    {
+        //AltEnter on factorial -> To local function
+        
+        //Then you can convert a local variable to the regular method using a corresponding context action
+
+
+        static void Main(string[] args)
+        {
+            Func<int, int> factorial = null;
+            factorial = x =>
+            {
+                if (x <= 1)
+                    return 1;
+
+                return x * int> factorial(x - 1);
+            };
+
+            Console.WriteLine(int> factorial(5));
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ ReSharper20171Cpp/JoinDeclration.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib> 
+using namespace std;
+
+int main()
+{
+		
+	//AltEnter on s -> Join declaration and assignment
+
+	string s;
+	// ... no use of s here ...
+	s = "what a waste";
+	
+	
+	
+}
