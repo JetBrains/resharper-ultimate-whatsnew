d918394761286d55e8e63ad59434b5cf12337ddb
Maarten Balliauw <maarten@maartenballiauw.be>
2017-06-06 09:55:02 +0200

    Add R# 2017.2 C#

---
+++ ReSharper20172CSharp/Csharp71.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+// ReSharper disable UnusedVariable
+
+namespace ReSharper20172CSharp
+{
+    public class Csharp71
+    {
+        public static async Task Main()
+        {
+            // C# 7.1 default literal: Inspection shows (List<string>) is redundant and can be removed.
+            // Alt+Enter, Remove Redundant type specification.
+            List<string> someList = default(List<string>);
+
+            var someString = Create<string>();
+            var someInt = Create<int>();
+        }
+
+        private static T Create<T>()
+        {
+            // C# 7.1 default literal: Inspection shows (List<string>) is redundant and can be removed.
+            // Alt+Enter, Remove Redundant type specification.
+            return default(T);
+        }
+    }
+}
