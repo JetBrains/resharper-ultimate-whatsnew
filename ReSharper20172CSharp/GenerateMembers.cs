ee0fce6346e4d90232857dbf35b04ce0d3bf98c2
Maarten Balliauw <maarten@maartenballiauw.be>
2017-06-06 11:10:04 +0200

    Generate members

---
+++ ReSharper20172CSharp/GenerateMembers.cs
@@ -0,0 +1,16 @@
+namespace ReSharper20172CSharp
+{
+    public interface IPerson
+    {
+        string Name { get; }
+    }
+    
+    public class GenerateMembers
+        : IPerson
+    {
+        // Generate members can make read-only interface members mutable.
+        // Alt+Insert to generate members, "Generate missing members",
+        // tick "Make properties mutable".
+        
+    }
+}
