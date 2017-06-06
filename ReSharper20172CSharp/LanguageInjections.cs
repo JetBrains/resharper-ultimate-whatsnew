0cadba80f21f549a286da7d828ecdb9f17c36a6b
Maarten Balliauw <maarten@maartenballiauw.be>
2017-06-06 11:46:43 +0200

    Injections

--- ReSharper20172CSharp/LanguageInjections.cs
+++ ReSharper20172CSharp/LanguageInjections.cs
@@ -5,19 +5,9 @@ namespace ReSharper20172CSharp
     {
         public void InjectionsHappenHere()
         {
-            // Language injection supports escaping path references
-            // Paste "C:\Projects\WebApp\bower.json" into the following string:
-            var path = "";
-
-            // Language injection supports injected XML
-            // Paste the following snippet into the string:
-            /*
-<?xml version="1.0" encoding="utf-8"?>
-<packages>
-  <package id="Newtonsoft.JSON" version="10.0.2" targetFramework="net462" />
-</packages>
-             */
-            var xml = "";
+            // Language injection supports XML.
+            // Alt+Enter on "Maarten works at JetBrains" and use convert to CDATA. 
+            var xml = "<person id=\"1\" name=\"Maarten\">Maarten works at JetBrains</person>";
         }
     }
 }
