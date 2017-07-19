1c53ed0c287a174dfd754f2bf3ad91a7402b9eec
Maarten Balliauw <maarten@maartenballiauw.be>
2017-07-19 11:10:02 +0200

    Rename file

--- ReSharper20172CSharp/InspectionUseVar.cs
+++ ReSharper20172CSharp/InspectionUseVar.cs
@@ -1,12 +1,13 @@
 ﻿// ReSharper disable UnusedVariable
+
 namespace ReSharper20172CSharp
 {
-    public class Csharp
+    public class InspectionUseVar
     {
         public void OutVarToType()
         {
             // Use context action to change out var to specify type directly
-            string value = "1";
+            var value = "42";
             if (int.TryParse(value, out var result))
             {
                 // ...
@@ -16,7 +17,7 @@ namespace ReSharper20172CSharp
         public void OutTypeToVar()
         {
             // Use context action to change out int to use var
-            string value = "1";
+            var value = "42";
             if (int.TryParse(value, out int result))
             {
                 // ...
