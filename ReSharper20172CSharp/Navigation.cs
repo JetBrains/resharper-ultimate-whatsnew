adb6ca43b175ee9ffcf094bcc3c37b35b05c38cb
Maarten Balliauw <maarten@maartenballiauw.be>
2017-07-19 09:50:04 +0200

    Add navigation example

---
+++ ReSharper20172CSharp/Navigation.cs
@@ -0,0 +1,47 @@
+﻿namespace ReSharper20172CSharp.Navigation
+{
+    public class Navigation
+    {
+        public void Demo()
+        {
+            // 1. Exact matches
+            // Use "Find Everywhere" (Ctrl+T) to find the type Service.
+            // Entering Service -> lots of other things found
+            // Entering "Service" (with the quotes) -> exact match
+
+            // 2. Word order
+            // Use "Find Everywhere" (Ctrl+T) to find the method that validates users.
+            // We forgot what it's called!
+            // Entering UserValid -> found
+            // Entering ValidUser -> found
+        }
+
+        public void FindService()
+        {
+        }
+
+        public void UserValid()
+        {
+        }
+    }
+
+    #region Because everyone likes regions to hide things...
+    
+    public class Service
+    {
+    }
+
+    public class UserService
+    {
+    }
+
+    public class IssueService
+    {
+    }
+
+    public class WorkItemService
+    {
+    }
+
+    #endregion
+}
