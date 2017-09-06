933d1e4b8fb0f08a4ab42e4b34650cb11a26f3e7
Maarten Balliauw <maarten@balliauw.be>
2017-09-06 10:27:47 +0200

    Add Generate Type patterns

---
+++ ReSharper20172CSharp/PatternMatching.cs
@@ -0,0 +1,65 @@
+using System;
+
+namespace ReSharper20172CSharp
+{
+    class PatternMatching
+    {
+        public IShape GenerateShape(string typeOfShape)
+        {
+            switch (typeOfShape)
+            {
+                case "circle":
+                    return new Circle();
+                case "rectangle":
+                    return new Rectangle();
+            }
+
+            return null;
+        }
+
+        public void InspectOldStyle()
+        {
+            var circle = GenerateShape("circle") as Circle; // Use pattern matching quick-fix
+            if (circle != null)
+            {
+                Console.WriteLine(circle.Radius);
+            }
+        }
+        
+        public void GenerateCode(IShape shape)
+        {
+            switch (shape)
+            {
+                // Alt+Insert here, Generate type patterns
+            }
+        }
+
+        public void ConvertToSwitch(IShape shape)
+        {
+            // Alt+Enter on if, convert to switch statement
+            if (shape is Circle circle)
+            {
+                // do circular stuff
+            }
+            else if (shape is Rectangle rectangle)
+            {
+                // do rectangular stuff
+            }
+        }
+        
+        public interface IShape
+        {
+        }
+
+        public class Circle : IShape
+        {
+            public int Radius { get; set; }
+        }
+
+        public class Rectangle : IShape
+        {
+            public int Length { get; set; }
+            public int Height { get; set; }
+        }
+    }
+}
