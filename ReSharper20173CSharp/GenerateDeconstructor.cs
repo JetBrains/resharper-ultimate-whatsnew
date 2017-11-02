97580d2ed72fdcea94cae3c43be688463e87c9f8
Maarten Balliauw <maarten@balliauw.be>
2017-11-02 10:07:40 +0100

    Add ReSharper 2017.3 project

---
+++ ReSharper20173CSharp/GenerateDeconstructor.cs
@@ -0,0 +1,11 @@
+﻿namespace ReSharper20173CSharp.GenerateDeconstructor
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        // Alt+Insert, Deconstructor, add deconstructor for select properties
+    }
+}
