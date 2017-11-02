9bb5f58d761371d70eb6c41f54e4bc2487e3299e
Maarten Balliauw <maarten@balliauw.be>
2017-11-02 11:06:30 +0100

    Find usages of deconstructor

---
+++ ReSharper20173CSharp/FindUsagesDeconstructor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ReSharper20173CSharp.Deconstructors
+{
+    public static class DateTimeExtensions
+    {
+        public static void Deconstruct(
+            this DateTime dateTime, out int year, out int month, out int day)
+        {
+            year = dateTime.Year;
+            month = dateTime.Month;
+            day = dateTime.Day;
+        }
+    }
+
+    public class FindUsagesDeconstructor
+    {
+        /// In <see cref="DateTimeExtensions.Deconstruct"/>, use Navigate To (Alt+`) to find usages of the extension method.
+
+        public void Usage1()
+        {
+            (int year, int month, int day) = DateTime.UtcNow;
+        }
+
+        public void Usage2()
+        {
+            (int year, int month, int day) = DateTime.UtcNow;
+        }
+    }
+}
