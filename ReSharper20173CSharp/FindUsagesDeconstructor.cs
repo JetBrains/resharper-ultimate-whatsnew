807e8ac12994842e709149fdea0fd712f4d0017e
Maarten Balliauw <maarten@balliauw.be>
2017-11-07 09:32:04 +0100

    Update deconstructor extension

--- ReSharper20173CSharp/FindUsagesDeconstructor.cs
+++ ReSharper20173CSharp/FindUsagesDeconstructor.cs
@@ -1,30 +1,38 @@
-﻿using System;
+﻿using System.Collections.Generic;
+// ReSharper disable CollectionNeverUpdated.Local
 
 namespace ReSharper20173CSharp.Deconstructors
 {
-    public static class DateTimeExtensions
+    public static class KeyValuePairExtensions
     {
-        public static void Deconstruct(
-            this DateTime dateTime, out int year, out int month, out int day)
+        public static void Deconstruct<TKey, TValue>(
+            this KeyValuePair<TKey, TValue> pair,
+            out TKey key, out TValue value)
         {
-            year = dateTime.Year;
-            month = dateTime.Month;
-            day = dateTime.Day;
+            key = pair.Key;
+            value = pair.Value;
         }
     }
 
     public class FindUsagesDeconstructor
     {
-        /// In <see cref="DateTimeExtensions.Deconstruct"/>, use Navigate To (Alt+`) to find usages of the extension method.
+        /// In <see cref="KeyValuePairExtensions.Deconstruct"/>, use Navigate To (Alt+`) to find usages of the extension method.
 
         public void Usage1()
         {
-            (int year, int month, int day) = DateTime.UtcNow;
+            var dictionary = new Dictionary<string, string>();
+            foreach (var (key, value) in dictionary)
+            {
+                // do something
+            }
         }
 
-        public void Usage2()
+        public void Usage2(Dictionary<string, int> dictionary)
         {
-            (int year, int month, int day) = DateTime.UtcNow;
+            foreach (var (key, value) in dictionary)
+            {
+                // do something
+            }
         }
     }
 }
