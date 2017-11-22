6328a995d4800c7293b5a53087cedd2d11ac522f
Maarten Balliauw <maarten@balliauw.be>
2017-11-22 09:18:16 +0100

    Wrap/chop long line

---
+++ ReSharper20173CSharp/Formatter/WrapChopLongLines.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReSharper20173CSharp.Deconstructors.Formatter
+{
+    public class WrapChopLongLines
+    {
+        public void Demo()
+        {
+            // AltEnter on the following line, use wrap/chop and show the difference between both
+            var reportHtml = CreateReport("359CD313-7C7E-49B2-A2AB-E12FDFD1319D", DateTimeOffset.UtcNow.AddMonths(-1), DateTimeOffset.UtcNow, useRichText: true);
+        }
+
+        public static string CreateReport(string customerId, DateTimeOffset from, DateTimeOffset to, bool useRichText)
+        {
+            return string.Empty;
+        }
+    }
+}
