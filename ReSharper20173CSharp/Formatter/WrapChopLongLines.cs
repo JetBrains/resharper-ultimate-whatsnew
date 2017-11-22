234b3e4da625a87865083db95398c511206c768b
Maarten Balliauw <maarten@balliauw.be>
2017-11-22 09:46:35 +0100

    Additional formatting demo

--- ReSharper20173CSharp/Formatter/WrapChopLongLines.cs
+++ ReSharper20173CSharp/Formatter/WrapChopLongLines.cs
@@ -10,6 +10,11 @@ namespace ReSharper20173CSharp.Deconstructors.Formatter
             var reportHtml = CreateReport("359CD313-7C7E-49B2-A2AB-E12FDFD1319D", DateTimeOffset.UtcNow.AddMonths(-1), DateTimeOffset.UtcNow, useRichText: true);
         }
 
+        // Select the following two properties, then Alt+Enter to format selection.
+        // Demonstrate the two versions (compact / spatious)
+        public string Name { get; set; }
+        public int Age { get; set; }
+
         public static string CreateReport(string customerId, DateTimeOffset from, DateTimeOffset to, bool useRichText)
         {
             return string.Empty;
