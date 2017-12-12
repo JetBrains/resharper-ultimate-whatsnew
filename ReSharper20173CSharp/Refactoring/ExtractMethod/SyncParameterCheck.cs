27e6a9de5ecaf0320dd237f2d2ab376c57d1331a
Maarten Balliauw <maarten@balliauw.be>
2017-12-12 09:32:52 +0100

    Extract method examples

---
+++ ReSharper20173CSharp/Refactoring/ExtractMethod/SyncParameterCheck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ReSharper20173CSharp.Refactoring.ExtractMethod
+{
+    class SyncParameterCheck
+    {
+        static async Task<int> GetMultiple(int secondsDelay)
+        {
+            if (secondsDelay < 0 || secondsDelay > 5)
+                throw new ArgumentOutOfRangeException("secondsDelay cannot exceed 5.");
+
+            // Select from here to end, Ctrl+R, M (Extract mehod).
+            // Choose local function, place at the end of GetMultiple method.
+            Console.WriteLine("Executing GetValueAsync...");
+            await Task.Delay(secondsDelay * 1000);
+            return secondsDelay * new Random().Next(2, 10);
+        }
+    }
+}
---
+++ ReSharper20173CSharp/Refactoring/ExtractMethod/TupleReturn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReSharper20173CSharp.Refactoring.ExtractMethod
+{
+    class TupleReturn
+    {
+        public void Method(string word)
+        {
+            // Select from here
+            (int index, bool found) = FindWord(word);
+            if (!found)
+                Console.WriteLine($"Unable to found word '{word}'");
+            // To here. Ctrl+R, M (Extract mehod), choose method, multiple return values (tuple)
+            if (found)
+                Console.WriteLine(index);
+        }
+        private static (int, bool) FindWord(string word) => (12, true);
+    }
+}
