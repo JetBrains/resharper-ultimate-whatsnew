d7a0c8c7d7400c1b9ccb4c31d4780e7d88d734fc
Maarten Balliauw <maarten@balliauw.be>
2017-12-14 09:59:16 +0100

    More local functions

---
+++ ReSharper20173CSharp/Refactoring/ExtractMethod/ExtractMethod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ReSharper20173CSharp.Refactoring.ExtractMethod
+{
+    class ExtractMethod
+    {
+        public Point GetPoint()
+        {
+            // We can now rewrite deconstructing declaration to assignment.
+            // Extract new method (not local function) from here:
+            (int x, int y, int z) = GetCoords();
+            Console.WriteLine($"Current position is: X={x}, Y={y}, Z={z}");
+            // To here
+            return new Point(x, y);
+        }
+
+        private (int x, int y, int z) GetCoords() => (0, 1, 2);
+
+        public class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+    }
+}
