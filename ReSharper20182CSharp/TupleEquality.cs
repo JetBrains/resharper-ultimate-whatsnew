66de803368773848346d8e416a5884ddcc4da204
Maarten Balliauw <maarten@balliauw.be>
2018-06-22 09:22:18 +0200

    TupleEquality

---
+++ ReSharper20182CSharp/TupleEquality.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ReSharper20182CSharp
+{
+    public class TupleEquality
+    {
+        private static (string firstname, string surname) GetName()
+        {
+            return ("Jason", "Isaacs");
+        }
+
+        public void Equality()
+        {
+            var a = ("Jason", "Isaacs");
+            var b = GetName();
+            
+            if (a == b)
+            {
+                // Seems our tuples are equal
+            }
+        }
+
+        public void EqualityWithNullables()
+        {
+            var a = ((DateTime?)null, (DateTime?)null);
+            var b = (DateTime.UtcNow, DateTime.UtcNow);
+
+            if (a == b)
+            {
+                // Seems our tuples are equal
+            }
+        }
+    }
+}
