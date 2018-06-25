dc7a0e3ddf31a036b9c02f3610b4fb6d18a3084a
Maarten Balliauw <maarten@balliauw.be>
2018-06-25 14:32:16 +0200

    Rider debugger stackframe demo

---
+++ Rider20182/Debugger/DebuggerStackFrames.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Rider20182
+{
+    internal class DebuggerStackFrames
+    {
+        // Improved presentation of stack frames (.ctor -> new T, .cctor -> static T, generic types with actual substitution) RIDER-11187 
+
+        static DebuggerStackFrames()
+        {
+            new DebuggerStackFrames();
+        }
+
+        public DebuggerStackFrames()
+        {
+            Run(null);
+        }
+
+        public static void Run(string[] args)
+        {
+            new MyClass<int, int>().DoSomething();
+        }
+
+        public static void SomeMethod<TValue1, TValue2, TValue3>()
+        {
+            SomeMethod<int, int>();
+        }
+
+        public static void SomeMethod<TValue1, TValue2>()
+        {
+            // Place breakpoint here, and compare call stack with previous versions
+            Console.WriteLine();
+        }
+    }
+    internal class MyClass<TKey, TValue>
+    {
+        public void DoSomething()
+        {
+            DebuggerStackFrames.SomeMethod<int, int, int>();
+        }
+    }
+}
---
+++ Rider20182/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rider20182
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DebuggerStackFrames.Run(args);
+        }
+    }
+}
