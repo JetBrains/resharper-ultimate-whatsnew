80cf150bad61f69e50caedb8b6ef773285ea17aa
Maarten Balliauw <maarten@balliauw.be>
2018-07-02 13:16:15 +0200

    Type constraint example

--- ReSharper20182CSharp/TypeConstraints.cs
+++ ReSharper20182CSharp/TypeConstraints.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ReSharper20182CSharp
 {
     public class TypeConstraints
     {
         public static IEnumerable<string> GetValues<T>()
-            where T : System.Enum
+            where T : struct, System.Enum
         {
             var enumType = typeof(T);
             var items = Enum.GetValues(enumType);
@@ -15,6 +16,25 @@ namespace ReSharper20182CSharp
             {
                 yield return Enum.GetName(enumType, item);
             }
+        }
+
+        private void CombineDelegates()
+        {
+            void Hello() => Console.WriteLine("Hello");
+            Action world = () => Console.WriteLine("World");
+
+            var helloWorld = Combine(Hello, world);
+
+            Func<bool> test = () => true;
+
+            // Will not compile - not the same Delegate type
+            // var helloWorld2 = Combine(Hello, test);
+        }
+
+        public static TDelegate Combine<TDelegate>(TDelegate source, TDelegate target)
+            where TDelegate : Delegate
+        {
+            return (TDelegate)Delegate.Combine(source, target);
         }
 
         int DoSomething<T>(T value) where T : unmanaged
