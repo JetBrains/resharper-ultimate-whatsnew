f298b4181df0bf0ad5c8141f04a298ec7713a662
Maarten Balliauw <maarten@maartenballiauw.be>
2018-10-30 09:27:23 +0100

    Inferred tuple element names

---
+++ ReSharper20183VBNet/InferredTupleElementNames.vb
@@ -0,0 +1,13 @@
+Public Class InferredTupleElementNames
+    Public Sub Example()
+        Dim firstName = "Maarten"
+        Dim lastName = "Balliauw"
+        Dim email = "maarten.balliauw@jetbrains.com"
+
+        Dim person = (firstName, lastName, email)
+
+        'In earlier VB versions, our "person" tuple would expose Item1, Item2, Item3
+        'In newer VB versions, the "person" tuple will have inferred named elements
+        Console.WriteLine($"E-mail for {person.firstName}: {person.lastName}")
+    End Sub
+End Class
