82b9f6de93fb95b7c825067ba0dc6b3be94908af
Maarten Balliauw <maarten@maartenballiauw.be>
2018-10-30 09:28:24 +0100

    Leading decimal separator

---
+++ ReSharper20183VBNet/LeadingDecimalSeparator.vb
@@ -0,0 +1,6 @@
+Public Class LeadingDecimalSeparator
+    Public Sub Example()
+        'This syntax would not work in older VB.NET
+        Dim number As Integer = &H_C305_F860
+    End Sub
+End Class
