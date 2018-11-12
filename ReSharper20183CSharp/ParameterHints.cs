35f85d939d5d39e2ad988cefb0a5b95e22ae5a67
Maarten Balliauw <maarten@maartenballiauw.be>
2018-11-12 09:18:48 +0100

    Parameter hints

---
+++ ReSharper20183CSharp/ParameterHints.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ReSharper20183CSharp
+{
+    [SuppressMessage("ReSharper", "ArgumentsStyleLiteral")]
+    public class ParameterHints
+    {
+        public IEnumerable<string> SplitWords(string sentence)
+        {
+            if (string.IsNullOrEmpty(sentence)) return Enumerable.Empty<string>();
+
+            return Regex.Split(sentence, "\\s");
+        }
+
+        public void NullAndBoolean()
+        {
+            var peopleService = new PeopleService();
+            var people = peopleService.Find("Maarten", null, false);
+        }
+
+        #region Clutter
+
+        public class Person
+        {
+            
+        }
+
+        public class PeopleService
+        {
+            public IEnumerable<Person> Find(string firstName, string lastName, bool caseSensitive)
+            {
+                return null;
+            }
+        }
+
+        #endregion
+    }
+}
