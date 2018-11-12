3241b4a582358691b05cfcfaa14d54cd6acc5c3c
Maarten Balliauw <maarten@maartenballiauw.be>
2018-11-12 09:44:11 +0100

    Add NoParameterHintsForThese

--- ReSharper20183CSharp/ParameterHints.cs
+++ ReSharper20183CSharp/ParameterHints.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace ReSharper20183CSharp
@@ -20,6 +21,20 @@ namespace ReSharper20183CSharp
         {
             var peopleService = new PeopleService();
             var people = peopleService.Find("Maarten", null, false);
+        }
+
+        public void NoParameterHintsForThese()
+        {
+            var range = Enumerable.Range(10, 20);
+            var minimum = Math.Min(1, 2);
+            
+            var person1 = new Person();
+            var person2 = new Person();
+            var areEqual = person1.Equals(person2);
+
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("Hello, ");
+            stringBuilder.Append("World!");
         }
 
         #region Clutter
