bbce8319bf764215f6fcf3af0e229671db8efeaa
Maarten Balliauw <maarten@maartenballiauw.be>
2018-11-26 12:39:53 +0100

    Detect formatting settings

---
+++ ReSharper20183CSharp/FormatterDetections/DetectFormatting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReSharper20183CSharp.FormatterDetections
+{
+    // ReSharper | Edit | Detect formatting settings
+    public class DetectFormatting {
+        public void MyMethod() {
+            // ...
+        }
+
+        public string CurrentHoliday() {
+            if (DateTime.Now.Month == 1 && DateTime.Now.Day == 1)
+                return "New Year's Day";
+            else
+                return null;
+        }
+    }
+}
