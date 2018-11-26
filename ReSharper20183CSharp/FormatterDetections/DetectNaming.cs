52cc55cc26162d735c8337c169d813571a0b7eab
Maarten Balliauw <maarten@maartenballiauw.be>
2018-11-26 12:44:46 +0100

    Detect Naming

---
+++ ReSharper20183CSharp/FormatterDetections/DetectNaming.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+namespace ReSharper20183CSharp.FormatterDetections
+{
+    public class DetectNaming
+    {
+        private string Name;
+        private static bool staticField = true;
+    }
+}
