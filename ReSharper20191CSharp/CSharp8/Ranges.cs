6e412bc535a4e13a44844b08052966927d9a23d9
Maarten Balliauw <maarten@maartenballiauw.be>
2019-03-04 09:56:29 +0100

    Initial 2019.1 commit

---
+++ ReSharper20191CSharp/CSharp8/Ranges.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReSharper20191CSharp
+{
+    public class Ranges
+    {
+        public void Simple()
+        {
+            //foreach (var item in 1..100)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+    }
+}
---
+++ ReSharper20191CSharp/RemoveUnusedReferences.cs
@@ -0,0 +1,21 @@
+﻿namespace ReSharper20191CSharp
+{
+    public class RemoveUnusedReferences
+    {
+        // 1. Make sure the project file contains a couple of NuGet references, e.g.:
+        /*
+            <ItemGroup>
+                <PackageReference Include = "Microsoft.EntityFrameworkCore" Version="3.0.0-preview.19074.3" />
+                <PackageReference Include = "Newtonsoft.Json" Version="12.0.1" />
+            </ItemGroup>
+        */
+
+        // 2. Build the project (to ensure packages are all restored)
+
+        // 3. Use the context menu on ReSharper20191CSharp, Optimize References... (Ctrl+Alt+Y)
+
+        // 4. See there are two unused NuGet references (Microsoft.EntityFrameworkCore and Newtonsoft.Json)
+
+        // 5. Remove one of them, verify the package is uninstalled.
+    }
+}
