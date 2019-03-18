11e66648d185c71731e741f56ba0c1b904712e8c
Maarten Balliauw <maarten@maartenballiauw.be>
2019-03-18 10:29:52 +0100

    Extra unused references

--- ReSharper20191CSharp/RemoveUnusedReferences.cs
+++ ReSharper20191CSharp/RemoveUnusedReferences.cs
@@ -5,6 +5,7 @@
         // 1. Make sure the project file contains a couple of NuGet references, e.g.:
         /*
             <ItemGroup>
+                <PackageReference Include="CsvHelper" Version="12.1.2" />
                 <PackageReference Include = "Microsoft.EntityFrameworkCore" Version="3.0.0-preview.19074.3" />
                 <PackageReference Include = "Newtonsoft.Json" Version="12.0.1" />
             </ItemGroup>
@@ -14,7 +15,7 @@
 
         // 3. Use the context menu on ReSharper20191CSharp, Optimize References... (Ctrl+Alt+Y)
 
-        // 4. See there are two unused NuGet references (Microsoft.EntityFrameworkCore and Newtonsoft.Json)
+        // 4. See there are three unused NuGet references (CsvHelper, Microsoft.EntityFrameworkCore and Newtonsoft.Json)
 
         // 5. Remove one of them, verify the package is uninstalled.
     }
