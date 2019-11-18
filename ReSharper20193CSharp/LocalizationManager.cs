33a474005efeeb81afe53ae83a7a3100463d2236
Maarten Balliauw <maarten@maartenballiauw.be>
2019-11-18 13:56:04 +0100

    Localization Manager

---
+++ ReSharper20193CSharp/LocalizationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReSharper20193CSharp
+{
+    public class LocalizationManager
+    {
+        public static void Example()
+        {
+            // 1. Open ReSharper | Windows | Localization Manager
+
+            // 2. Add a new culture and translate the following resource
+            var helloWorld1 = Labels.HelloWorld;
+
+            // 3. Add a new resource based on this string. Use Refactor This or Alt+Enter and move to resource.
+            Console.WriteLine("Another label");
+
+            // 4. Rename this resource using the Localization Manager or Refactor This
+            var helloWorld2 = Labels.HelloWorld;
+
+            // 5. Safe delete UnusedResource
+            // Labels.HelloWorld
+        }
+    }
+}
