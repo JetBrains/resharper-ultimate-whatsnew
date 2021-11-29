c5b1c724018c88d0ffb56ac7a3a5d7ae70ab9289
JetBrains User <alexander.kurakin@jetbrains.com>
2021-11-29 16:36:35 +0100

    Simplify the pattern matching syntax with dotted access New Gutter Marks

---
+++ 2021.3/CS10/PatternMatching/SimplifyPatternMatchingDottedAccess.cs
@@ -0,0 +1,28 @@
+﻿namespace CS10.PatternMatching;
+
+public class SimplifyPatternMatchingDottedAccess
+{
+    public void Demo()
+    {
+
+        var user1 = new User();
+        if (user1 is { Tags.Count: 0 }) // CA "Split into nested patterns" over dot
+        {
+            // ...
+        }
+
+
+
+
+        var user = new User();
+        if (user is { Tags: { Count: 0 } }) 
+        {
+            // ...
+        }
+    }
+
+    public class User
+    {
+        public List<string> Tags { get; } = new();
+    }
+}
---
+++ 2021.3/NewGutterMarks/Class1.cs
@@ -0,0 +1,32 @@
+﻿// ReSharper disable All
+
+
+
+
+namespace NewGutterMarks;
+
+internal interface ITopInterface {
+    public void Foo() {}
+}
+internal class TopClass : ITopInterface {
+    void ITopInterface.Foo() { }
+    protected virtual void Goo() { }
+}
+internal class ClassImpl : TopClass, ITopInterface {
+    protected override void Goo() { }
+}
+
+
+
+
+internal struct MyStruct : ITopInterfaceImpl {
+    public void Foo() { }
+}
+
+internal class ClassOneMoreImpl : TopClass {
+    protected override void Goo() { }
+}
+
+internal interface ITopInterfaceImpl : ITopInterface {
+    void ITopInterface.Foo() { }
+}
