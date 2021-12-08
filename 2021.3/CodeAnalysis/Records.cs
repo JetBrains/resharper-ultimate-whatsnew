e2a7945290117b565d0fdb44ed295c7b2cd18e7c
Maarten Balliauw <maarten@maartenballiauw.be>
2021-12-08 17:05:56 +0100

    Records

---
+++ 2021.3/CodeAnalysis/Records.cs
@@ -0,0 +1,23 @@
+﻿// ReSharper disable once CheckNamespace
+// ReSharper disable UnusedType.Global
+namespace CSCodeAnalysis.Records;
+
+class Code
+{
+    void Example()
+    {
+        // 1. Alt+Enter on Person, change to constructor
+        var person = new Person { Name = "Maarten", Age = "37" };
+    }
+}
+
+// 2. Alt+Enter on class, make record
+public class Person
+{
+    // 3. Remove unused constructor
+    // 4. Make primary constructor
+    // 5. Change properties to init accessor (on set)
+    // 6. Remove explicit properties
+    public string Name { get; set; }
+    public string Age { get; set; }
+}
