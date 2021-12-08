cf23684ad512306690582ffec45f637ddc3a9826
Maarten Balliauw <maarten@maartenballiauw.be>
2021-12-08 17:21:19 +0100

    Arithmetics

---
+++ 2021.3/CodeAnalysis/Arithmetics.cs
@@ -0,0 +1,49 @@
+﻿// ReSharper disable once CheckNamespace
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedVariable
+
+namespace CSCodeAnalysis;
+
+class Arithmetics
+{
+    public void ExampleWithInt1()
+    {
+        var maximumValue = int.MaxValue;
+
+        var overflowingValue = maximumValue + 1;
+        var uncheckedValue = unchecked(maximumValue + 1);
+    }
+
+    public void ExampleWithInt2(int value)
+    {
+        var absoluteValue = Math.Abs(value);
+        if (absoluteValue < 0)
+        {
+            // ...
+        }
+    }
+
+    public void ExampleWithColors(ConsoleColor color)
+    {
+        switch (color)
+        {
+            case ConsoleColor.Red:
+                Console.WriteLine("Color is red!");
+                break;
+
+            case ConsoleColor.Blue:
+                Console.WriteLine("Color is blue!");
+
+                if (color == ConsoleColor.Yellow)
+                {
+                    Console.WriteLine("Color is yellow!");
+                }
+
+                break;
+
+            case ConsoleColor.Green:
+                Console.WriteLine("Color is green!");
+                break;
+        }
+    }
+}
