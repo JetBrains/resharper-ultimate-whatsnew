2aa5c8c74a8801fbd149cbd3ac29c6cb0156ab54
Maarten Balliauw <maarten@maartenballiauw.be>
2021-12-08 16:52:06 +0100

    Some NRT examples

---
+++ 2021.3/CodeAnalysis/FixNRTs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable enable
+namespace CSCodeAnalysis;
+
+// Alt+Enter on MiddleName, change to string?
+record Person(string FirstName, string LastName, string MiddleName = null);
+
+public class HelloWorld
+{
+    public void SayHello(string name)
+    {
+        // Alt+Enter on the next line, make parameter nullable
+        if (name == null)
+        {
+            Console.WriteLine("Hello! 👋");
+        }
+        else
+        {
+            Console.WriteLine($"Hello, {name}! 👋");
+        }
+    }
+}
