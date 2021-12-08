ea77535b8b3c1391adc80167277858daf8d9d488
Maarten Balliauw <maarten@maartenballiauw.be>
2021-12-08 17:12:05 +0100

    Locks

---
+++ 2021.3/CodeAnalysis/Locks.cs
@@ -0,0 +1,39 @@
+﻿// ReSharper disable once CheckNamespace
+// ReSharper disable UnusedType.Global
+namespace CSCodeAnalysis;
+
+class Locks
+{
+    private static readonly object Lock1 = new();
+    private static readonly object Lock2 = new();
+
+    public void SomeMethod1()
+    {
+        lock (Lock1)
+        {
+            // do things
+
+            lock (Lock2)
+            {
+                // do other things
+            }
+        }
+    }
+
+    public void SomeMethod2()
+    {
+        lock (Lock2)
+        {
+            // do things
+            SomeMethod3();
+        }
+    }
+
+    private void SomeMethod3()
+    {
+        lock (Lock1)
+        {
+            // do things
+        }
+    }
+}
